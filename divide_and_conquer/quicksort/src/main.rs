d106f723e419f453c99b3518b7da3793962ada8a
MSAugdal <mathias.s.augdal@gmail.com>
2024-07-02 16:07:02 +0200

    ✨ Implemented: quicksort
    implemented quicksort as described in Grokking Algorithms page 134

--- divide_and_conquer/quicksort/src/main.rs
+++ divide_and_conquer/quicksort/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let list = [2, 1, 5, 14];
+    let sorted_list = [1, 2, 5, 14];
+    assert_eq!(quicksort(Vec::from(list)), Vec::from(sorted_list));
+}
+
+fn quicksort<T: std::cmp::PartialOrd + std::clone::Clone>(list: Vec<T>) -> Vec<T> {
+    if list.len() < 2 {
+        list
+    } else {
+        let pivot = &list[0];
+        let less: Vec<T> = list.clone().into_iter().filter(|x| x.lt(&pivot)).collect();
+        let greater: Vec<T> = list.clone().into_iter().filter(|x| x.gt(&pivot)).collect();
+        [
+            quicksort(less).as_slice(),
+            &[pivot.clone()],
+            quicksort(greater).as_slice(),
+        ]
+        .concat()
+    }
 }
